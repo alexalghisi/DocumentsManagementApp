bfa9e48d6ca4ef445103ce6c25aad079a4a89bc1
Alghisi Alessandro <alessandro@sectorlabs.ro>
2019-06-05 15:15:47 +0300

    Login WORKS

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,21 +1,104 @@
 import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableHighlight,
+  Alert
+} from 'react-native';
+import Colors from '../constants/colors';
+import {widthPercentageToDP as wp } from 'react-native-responsive-screen';
 
-class HomeScreen extends Component {
-  static navigationOptions = {
-    title: 'Home',
+export default class LoginView extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email   : '',
+      password: '',
+    };
+  }
+
+  onClickListener = (viewId) => {
+    Alert.alert("Alert", "Button pressed "+viewId);
   };
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.inputs}
+              placeholder="Email"
+              keyboardType="email-address"
+              underlineColorAndroid='transparent'
+              onChangeText={(email) => this.setState({email})}/>
+        </View>
+
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.inputs}
+              placeholder="Parola"
+              secureTextEntry={true}
+              underlineColorAndroid='transparent'
+              onChangeText={(password) => this.setState({password})}/>
+        </View>
+
+        <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => this.onClickListener('login')}>
+          <Text style={styles.loginText}>Login</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('restore_password')}>
+            <Text>Forgot your password?</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('register')}>
+            <Text>Register</Text>
+        </TouchableHighlight>
       </View>
     );
   }
 }
 
-export default HomeScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.inputBackgorundColor,
+  },
+  inputContainer: {
+      borderRadius:30,
+      borderBottomWidth: 1,
+      width: wp('84.5%'),
+      height:45,
+      marginBottom:20,
+      flexDirection: 'row',
+      alignItems:'center'
+  },
+  inputs:{
+      height:45,
+      marginLeft:16,
+      flex:1,
+  },
+  inputIcon:{
+    width:30,
+    height:30,
+    marginLeft:15,
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width: wp('80%'),
+    borderRadius:3,
+  },
+  loginButton: {
+    backgroundColor: Colors.backgroundColor,
+  },
+  loginText: {
+    color: Colors.loginButtonColor
+  }
+});
---
+++ src/constants/colors.js
@@ -0,0 +1,6 @@
+export default {
+  loginButtonColor: '#eceff5',
+  borderBottomColor: '#FFFFFF',
+  backgroundColor: '#42A5F5',
+  inputBackgorundColor: '#FFF'
+};
