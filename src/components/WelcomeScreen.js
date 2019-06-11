14f4a21d4d5790c87285d0e3918a92499ab292f2
Alghisi Alessandro <alessandro@sectorlabs.ro>
2019-06-11 18:11:22 +0300

    Added list

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button, View, Text } from 'react-native';
+import Table from './Table';
 
 class WelcomeScreen extends Component {
   static navigationOptions = {
@@ -8,7 +9,8 @@ class WelcomeScreen extends Component {
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Table/>
         <Text>Welcome</Text>
       </View>
     );
