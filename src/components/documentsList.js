3e12c2dfe85f5c1d5b05ef423262f1470a39a463
Alghisi Alessandro <alessandro@sectorlabs.ro>
2019-07-05 18:50:17 +0300

    Using hooks in the hoc :)
    && fix styles

--- src/components/documentsList.js
+++ src/components/documentsList.js
@@ -1,11 +1,9 @@
-import React, { Component } from "react";
-import {
-  Text,
-  View,
-  FlatList,
-  Image
-} from "react-native";
-import styles from '../styles/documentListStyles';
+import React, { useEffect } from "react";
+import { Text, View, FlatList, Image } from "react-native";
+import { Dimensions as ScreenDimensions, StyleSheet } from "react-native";
+import Colors from "../constants/colors";
+import Dimensions from "../constants/dimensions";
+
 import withFireBase from "./withFirebase";
 
 const headerImageUri =
@@ -16,40 +14,33 @@ const getDocuments = (data, numColumns) => {
 
   // Add the mandatory key for each item
   // displayed in the FlatList.
-  data.map((item, index) => item.key=index);
+  data.map((item, index) => (item.key = index));
 
   let numberOfElementsLastRow = data.length - numberOfFullRows * numColumns;
-  while (
-    numberOfElementsLastRow !== numColumns &&
-    numberOfElementsLastRow !== 0
-  ) {
-    data.push({ key: `blank-${numberOfElementsLastRow}`, empty: true });
-    numberOfElementsLastRow++;
+  if (numberOfElementsLastRow) {
+    while (numberOfElementsLastRow !== numColumns) {
+      data.push({ key: `blank-${numberOfElementsLastRow}`, empty: true });
+      ++numberOfElementsLastRow;
+    }
   }
   return data;
 };
 
-
-
-const DocumentsList = (props) => {
+const DocumentsList = props => {
   // Fetch data from Firebase.
-  React.useEffect(() => {
-    props.getFirebaseData();
-  });
+  useEffect(() => {
+    props.fetchData();
+  }, []);
 
   // Render function used by FlatList.
-  const renderItem = item => {
-    item = item.item;
+  const renderItem =  ({ item })  => {
     if (item.empty === true) {
       return <View style={[styles.item, styles.itemInvisible]} />;
     }
 
     return (
       <View style={styles.item}>
-        <Image
-          style={{ width: "100%", height: "70%" }}
-          source={{ uri: item.imageURI }}
-        />
+        <Image style={styles.item} source={{ uri: item.imageURI }} />
         <Text style={[styles.itemText, styles.typeTextStyle]}>{item.type}</Text>
         <Text style={[styles.itemText, styles.dateTextStyle]}>
           {item.expire}
@@ -58,10 +49,7 @@ const DocumentsList = (props) => {
     );
   };
 
-
-  const { numColumns } = props;
-  const { items } = props;
-
+  const { numColumns, items } = props;
   return (
     <View style={styles.viewContainer}>
       <React.Fragment>
@@ -75,8 +63,48 @@ const DocumentsList = (props) => {
       </React.Fragment>
     </View>
   );
-
 };
+
+let width = ScreenDimensions.get("window").width;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.containerBackgroundColor
+  },
+  headerImage: {
+    height: 80,
+    width: width,
+    marginBottom: Dimensions.marginBottom
+  },
+  viewContainer: {
+    backgroundColor: Colors.containerBackgroundColor
+  },
+  typeTextStyle: {
+    fontSize: Dimensions.primaryFontSize,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  dateTextStyle: {
+    fontSize: Dimensions.secondaryFontSize
+  },
+  item: {
+    height: 150,
+    width: 100,
+    backgroundColor: Colors.cardBackgroundColor,
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    margin: 1,
+    borderRadius: 3
+  },
+  itemInvisible: {
+    backgroundColor: "transparent"
+  },
+  itemText: {
+    color: Colors.textColor
+  }
+});
 
 DocumentsList.defaultProps = {
   numColumns: 3
--- src/components/withFirebase.js
+++ src/components/withFirebase.js
@@ -1,30 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { db } from "../config";
 
 let itemsRef = db.ref("/data");
 
 const withFireBase = WrappedComponent => {
-  const HOC = (props) => {
-    const [ items ] = React.useState(0);
+    const FirebaseConnector = (props) => {
+      const [ data, setData ] = useState([]);
 
-    const fetchData = () => {
-      itemsRef.once("value", snapshot => {
-        let data = snapshot.val();
-        this.setState({ items: Object.values(data) });
-      });
+        const fetchData = () => {
+          itemsRef.once("value", snapshot => {
+            const items = snapshot.val();
+            setData(Object.values(items));
+            return items;
+          });
+        };
+
+        return (
+            <WrappedComponent
+                {...props}
+                fetchData={fetchData}
+                items={data}
+            />
+        );
     };
 
-    return (
-      <WrappedComponent
-        {...props}
-        getFirebaseData={fetchData}
-        items={items}
-      />
-    );
-  };
-
-  return HOC;
+    return FirebaseConnector;
 };
 
 export default withFireBase;
-
